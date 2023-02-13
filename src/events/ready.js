49b6b0e612d10d39450c508e66f999d195321d02
defnot001 <marc.miller994@gmail.com>
2023-02-13 14:37:16 +0100

    changed formatting

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,8 +1,9 @@
-import { Event } from "djs-handlers";
 import { ActivityType } from "discord.js";
+import { Event } from "djs-handlers";
 
 export default new Event("ready", (client) => {
   console.log(`Logged in as ${client.user.tag}!`);
+
   client.user.setActivity("your mom", {
     type: ActivityType.Watching,
   });
