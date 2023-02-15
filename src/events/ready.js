e2fa34afc5b3ef64bc6454f306b02dd33bc476f2
ANDREJ6693 <andrej.vencelj.007@gmail.com>
2023-02-15 21:59:05 +0100

    added activity status in config

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,10 +1,27 @@
-import { ActivityType } from "discord.js";
+import { config } from "../config/config.js";
 import { Event } from "djs-handlers";
 
 export default new Event("ready", (client) => {
   console.log(`Logged in as ${client.user.tag}!`);
 
-  client.user.setActivity("your mom", {
-    type: ActivityType.Watching,
+  if (!config.activityStatus[0] && config.activityStatus[0] !== 0) {
+    return;
+  }
+
+  if (
+    config.activityStatus[0] > 5 ||
+    config.activityStatus[0] < 0 ||
+    config.activityStatus[0] === 4 ||
+    typeof config.activityStatus[0] !== "number" ||
+    typeof config.activityStatus[1] !== "string"
+  ) {
+    return console.log("Invalid activity status!");
+  }
+
+  client.user.setActivity(config.activityStatus[1], {
+    type: config.activityStatus[0],
   });
+  console.log(
+    `Set the activity status to: Type[${config.activityStatus[0]}] | Content[${config.activityStatus[1]}]`
+  );
 });
