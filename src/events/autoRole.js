38d43d243efe5119e74fe8774e5818248ec3ad95
ANDREJ6693 <andrej.vencelj.007@gmail.com>
2023-05-09 18:27:29 +0200

    small fix for auto role err catching

--- src/events/autoRole.js
+++ src/events/autoRole.js
@@ -2,21 +2,23 @@ import { Event } from "djs-handlers";
 import { config } from "../config/config.js";
 
 export default new Event("guildMemberAdd", (member) => {
-  if (!config.autoJoinRole) return;
-
-  const autorole = member.guild.roles.cache.get(config.autoJoinRole);
-
-  if (!autorole) {
-    return console.log(`Auto role not found: ${config.autoJoinRole}.`);
-  }
-
   try {
+    if (!config.autoJoinRole) return;
+
+    const autorole = member.guild.roles.cache.get(config.autoJoinRole);
+
+    if (!autorole) {
+      return console.log(`Auto role not found: ${config.autoJoinRole}.`);
+    }
+
     member.roles.add(autorole);
 
     console.log(
       `Automatically assigned role ${autorole.name} to ${member.user.username}.`
     );
   } catch (err) {
-    console.error(err);
+    console.error(
+      `Something went wrong trying to assign the auto role: ${err}`
+    );
   }
 });
